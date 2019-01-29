227b74022dee28add08db5951a55c024c8034096
Ryan Henderson <ryan.ev.henderson@gmail.com>
2019-01-29 08:38:43 -0600

    Update BlockMessage.java

--- blocks/src/block1/BlockMessage.java
+++ blocks/src/block1/BlockMessage.java
@@ -1,4 +1,5 @@
 package block1;
+
 //nt out block letters.
 //lets use a class f
 //create code to prior the message
@@ -10,28 +11,20 @@ package block1;
 class BlockMessage {
 	private String name;
 	private String message;
-	
-	
+
 	public BlockMessage(String name, String message) {
-	  this.name = name;
-	  this.message = message;
+		this.name = name;
+		this.message = message;
 	}
-	public String exclamation()
-	{
-	  return "  !  \n"+
-	         "  !  \n"+
-	         "  !  \n"+
-	         "     \n"+
-	         "  !  \n";
+
+	public String exclamation() {
+		return "  !  \n" + "  !  \n" + "  !  \n" + "     \n" + "  !  \n";
 	}
-	public String period()
-	{
-	  return "     \n"+
-	         "     \n"+
-	         "     \n"+
-	         " ... \n"+
-	         " ... \n";
+
+	public String period() {
+		return "     \n" + "     \n" + "     \n" + " ... \n" + " ... \n";
 	}
+
 	public String question()
 	{
 	  return "  ?? \n"+
@@ -39,154 +32,82 @@ class BlockMessage {
 	         "   ? \n"+
 	         "     \n"+
 	         "   ? \n";
-	public String D()
-	{
- 	  return 
-	 "  DDDDD    \n"+
-         "  D    D   \n"+
-         "  D    D   \n"+
-         "  D    D   \n"+
-         "  DDDDD    \n";
-	} 
-	public String E()
-	{
-  	  return 
-	 "  EEEEEE   \n"+
-         "  E        \n"+
-         "  EEEEEE   \n"+
-         "  E        \n"+
-         "  EEEEEE   \n";
-	} 
-	public String F()
-	{
-  	  return 
-	 "  FFFFFFF  \n"+
-         "  F        \n"+
-         "  FFFFFF   \n"+
-         "  F        \n"+
-         "  F        \n";
-	} 
+
+	public String D() {
+		return "  DDDDD    \n" + "  D    D   \n" + "  D    D   \n" + "  D    D   \n" + "  DDDDD    \n";
 	}
 
+	public String E() {
+		return "  EEEEEE   \n" + "  E        \n" + "  EEEEEE   \n" + "  E        \n" + "  EEEEEE   \n";
+	}
+
+	public String F() {
+		return "  FFFFFFF  \n" + "  F        \n" + "  FFFFFF   \n" + "  F        \n" + "  F        \n";
+	}
+
+	}
 
 	public String vendeta() {
-		return "VVVVV \n"
-				+" \n"
-				+ "VVVVV \n"
-				+" \n"
-				+ "VVVVV \n";
+		return "VVVVV \n" + " \n" + "VVVVV \n" + " \n" + "VVVVV \n";
 	}
+
 	public String whyMe() {
-		return "WWWWW \n"
-				+ " \n"
-				+ "WWWWW \n"
-				+ " \n"
-				+ "WWWWW \n";
+		return "WWWWW \n" + " \n" + "WWWWW \n" + " \n" + "WWWWW \n";
 	}
+
 	public String xylophone() {
-		return "XXXXX \n"
-				+ " \n"
-				+ "XXXXX \n"
-				+ " \n"
-				+ "XXXXX \n";
-	}
-	
-
-
-	public String pLetter()
-	  {
-	    return "   pppppp    \n"+
-	           "   ppp   p \n"+
-	           "   ppp   p \n"+
-	           "   pppppp       \n"+
-	           "   ppp   \n";
-	  } 
-	  public String qLetter()
-	  {
-		  return "   qqqqqq    \n"+ 
-				 " qq      qq  \n"+
-				 " qq   qq qq  \n"+
-				 " qq    qqq   \n"+
-				 "   qqqqq  q  \n";
-	  }
-	  public String rLetter()
-	  {
-		  return " rrrrrrrrr   \n"+
-				 " rr      rr  \n"+
-				 " rr     rrr  \n"+
-				 " rrrrrrrr    \n"+
-				 " rr     rr    \n"+
-				 " rr     rr    \n";
-	  }
+		return "XXXXX \n" + " \n" + "XXXXX \n" + " \n" + "XXXXX \n";
 	}
 
-	  public String blockM(){
-		  return
-				  "M       M\n"+
-				  "M M   M M\n"+
-				  "M  M M  M\n"+
-				  "M   M   M\n"+
-				  "M       M\n";
-	  }
-	  public String blockN(){
-		  return  "N    N\n"+
-				      "NN   N\n"+
-				      "N N  N\n"+
-				      "N  N N\n"+
-				      "N   NN\n";
-				  
-	  }
-	  public String blockO(){
-		  return
-				  " OOO\n"+
-				  "O   O\n"+
-				  "O   O\n"+
-				  "O   O\n"+
-				  " OOO\n";
-	  }
+	public String pLetter() {
+		return "   pppppp    \n" + "   ppp   p \n" + "   ppp   p \n" + "   pppppp       \n" + "   ppp   \n";
+	}
 
-	  public String Y()
-	  {
-	    return "  Y    Y   \n"+
-	           "   Y  Y    \n"+
-	           "    Y      \n"+
-	           "    Y      \n"+
-	           "    Y      \n";
-	  } 
-	  public String Z()
-	  {
-	    return "  ZZZZZ \n"+
-	           "     Z  \n"+
-	           "    Z   \n"+
-	           "   Z    \n"+
-	           "  ZZZZZ \n";
-	  } 
+	public String qLetter() {
+		return "   qqqqqq    \n" + " qq      qq  \n" + " qq   qq qq  \n" + " qq    qqq   \n" + "   qqqqq  q  \n";
+	}
+
+	public String rLetter() {
+		return " rrrrrrrrr   \n" + " rr      rr  \n" + " rr     rrr  \n" + " rrrrrrrr    \n" + " rr     rr    \n"
+				+ " rr     rr    \n";
+	}
+
+	}
+
+	public String blockM() {
+		return "M       M\n" + "M M   M M\n" + "M  M M  M\n" + "M   M   M\n" + "M       M\n";
+	}
+
+	public String blockN() {
+		return "N    N\n" + "NN   N\n" + "N N  N\n" + "N  N N\n" + "N   NN\n";
+
+	}
+
+	public String blockO() {
+		return " OOO\n" + "O   O\n" + "O   O\n" + "O   O\n" + " OOO\n";
+	}
+
+	public String Y() {
+		return "  Y    Y   \n" + "   Y  Y    \n" + "    Y      \n" + "    Y      \n" + "    Y      \n";
+	}
+
+	public String Z() {
+		return "  ZZZZZ \n" + "     Z  \n" + "    Z   \n" + "   Z    \n" + "  ZZZZZ \n";
+	}
 
 }
-public String blockJ(){
-    return
-        "JJJJJ\n"+
-        "   J  \n"+
-        "   J \n"+
-        "J  J \n"+
-        "JJJJ \n";
-  }
-  public String blockK(){
-    return
-      "K   K\n"+ 
-      "K KK \n"+
-      "KK   \n"+
-      "K KK \n"+
-      "K   K\n";
-  }
-  public String blockL(){
-    return
-      "L    \n"+
-      "L    \n"+
-      "L    \n"+
-      "L    \n"+
-      "LLLLL\n";
-  }
+
+	public String blockJ() {
+		return "JJJJJ\n" + "   J  \n" + "   J \n" + "J  J \n" + "JJJJ \n";
+	}
+
+	public String blockK() {
+		return "K   K\n" + "K KK \n" + "KK   \n" + "K KK \n" + "K   K\n";
+	}
+
+	public String blockL() {
+		return "L    \n" + "L    \n" + "L    \n" + "L    \n" + "LLLLL\n";
+	}
 
 	public String a() {
 		String ret = "";
@@ -197,7 +118,7 @@ public String blockJ(){
 		ret += "A   A\n";
 		return ret;
 	}
-	
+
 	public String b() {
 		String ret = "";
 		ret += "BBBB \n";
@@ -207,7 +128,7 @@ public String blockJ(){
 		ret += "BBBB \n";
 		return ret;
 	}
-	
+
 	public String c() {
 		String ret = "";
 		ret += "  CCC\n";
@@ -215,9 +136,9 @@ public String blockJ(){
 		ret += "C    \n";
 		ret += " C   \n";
 		ret += "  CCC\n";
-		return ret;	
+		return ret;
 	}
-	
+
 	public String d() {
 		String ret = "";
 		ret += "DD   \n";
@@ -227,7 +148,7 @@ public String blockJ(){
 		ret += "DD   \n";
 		return ret;
 	}
-	
+
 	public String e() {
 		String ret = "";
 		ret += "EEEEE\n";
@@ -237,7 +158,7 @@ public String blockJ(){
 		ret += "EEEEE\n";
 		return ret;
 	}
-	
+
 	public String f() {
 		String ret = "";
 		ret += "FFFFF\n";
@@ -247,7 +168,7 @@ public String blockJ(){
 		ret += "F    \n";
 		return ret;
 	}
-	
+
 	public String g() {
 		String ret = "";
 		ret += " GGG \n";
@@ -257,7 +178,7 @@ public String blockJ(){
 		ret += " GGG \n";
 		return ret;
 	}
-	
+
 	public String h() {
 		String ret = "";
 		ret += "H   H\n";
@@ -267,7 +188,7 @@ public String blockJ(){
 		ret += "H   H\n";
 		return ret;
 	}
-	
+
 	public String i() {
 		String ret = "";
 		ret += "IIIII\n";
@@ -277,7 +198,7 @@ public String blockJ(){
 		ret += "IIIII\n";
 		return ret;
 	}
-	
+
 	public String j() {
 		String ret = "";
 		ret += "JJJJJ\n";
@@ -287,7 +208,7 @@ public String blockJ(){
 		ret += " J   \n";
 		return ret;
 	}
-	
+
 	public String k() {
 		String ret = "";
 		ret += "K   K\n";
@@ -297,7 +218,7 @@ public String blockJ(){
 		ret += "K   K\n";
 		return ret;
 	}
-	
+
 	public String l() {
 		String ret = "";
 		ret += "L    \n";
@@ -307,7 +228,7 @@ public String blockJ(){
 		ret += "LLLLL\n";
 		return ret;
 	}
-	
+
 	public String m() {
 		String ret = "";
 		ret += "M   M\n";
@@ -317,7 +238,7 @@ public String blockJ(){
 		ret += "M   M\n";
 		return ret;
 	}
-	
+
 	public String n() {
 		String ret = "";
 		ret += "N   N\n";
@@ -327,7 +248,7 @@ public String blockJ(){
 		ret += "N   N\n";
 		return ret;
 	}
-	
+
 	public String o() {
 		String ret = "";
 		ret += "  O  \n";
@@ -337,7 +258,7 @@ public String blockJ(){
 		ret += "  O  \n";
 		return ret;
 	}
-	
+
 	public String p() {
 		String ret = "";
 		ret += "PPPP \n";
@@ -347,7 +268,7 @@ public String blockJ(){
 		ret += "P    \n";
 		return ret;
 	}
-	
+
 	public String q() {
 		String ret = "";
 		ret += " QQQ \n";
@@ -357,7 +278,7 @@ public String blockJ(){
 		ret += "    Q\n";
 		return ret;
 	}
-	
+
 	public String r() {
 		String ret = "";
 		ret += "RRRR \n";
@@ -367,7 +288,7 @@ public String blockJ(){
 		ret += "R   R\n";
 		return ret;
 	}
-	
+
 	public String s() {
 		String ret = "";
 		ret += " SSS \n";
@@ -377,7 +298,7 @@ public String blockJ(){
 		ret += " SSS \n";
 		return ret;
 	}
-	
+
 	public String t() {
 		String ret = "";
 		ret += "TTTTT\n";
@@ -387,7 +308,7 @@ public String blockJ(){
 		ret += "  T  \n";
 		return ret;
 	}
-	
+
 	public String u() {
 		String ret = "";
 		ret += "U   U\n";
@@ -397,7 +318,7 @@ public String blockJ(){
 		ret += " UUU \n";
 		return ret;
 	}
-	
+
 	public String v() {
 		String ret = "";
 		ret += "V   V\n";
@@ -407,7 +328,7 @@ public String blockJ(){
 		ret += "  V  \n";
 		return ret;
 	}
-	
+
 	public String w() {
 		String ret = "";
 		ret += "W W W\n";
@@ -417,7 +338,7 @@ public String blockJ(){
 		ret += " W W \n";
 		return ret;
 	}
-	
+
 	public String x() {
 		String ret = "";
 		ret += "X   X\n";
@@ -427,7 +348,7 @@ public String blockJ(){
 		ret += "X   X\n";
 		return ret;
 	}
-	
+
 	public String y() {
 		String ret = "";
 		ret += "Y   Y\n";
@@ -437,7 +358,7 @@ public String blockJ(){
 		ret += "  Y  \n";
 		return ret;
 	}
-	
+
 	public String z() {
 		String ret = "";
 		ret += "ZZZZZ\n";
@@ -447,5 +368,5 @@ public String blockJ(){
 		ret += "ZZZZZ\n";
 		return ret;
 	}
- 
+
 }
