27aff369607dc0d2737a147c1f43084bdb21a318
Ryan Henderson <ryan.ev.henderson@gmail.com>
2019-01-29 08:58:14 -0600

    Update Main.java

--- blocks/src/block1/Main.java
+++ blocks/src/block1/Main.java
@@ -10,48 +10,33 @@ import blocks.BlockMessage;
 //allow a message to be printed normally, and with block letter
 //write a method for each letter
 public class Main {
-  public static void main(String[] args) {
-    BlockMessage myMessage = new BlockMessage("first","!");
-    //BlockMessage myMessage = new BlockMessage("first","Y");
-    BlockMessage myMessage2 = new BlockMessage("first","Z");
-    System.out.println(myMessage.exclamation());
-    System.out.println(myMessage.period());
-    System.out.println(myMessage.question());
+	public static void main(String[] args) {
+		BlockMessage myMessage = new BlockMessage("first", "!");
+		// BlockMessage myMessage = new BlockMessage("first","Y");
+		BlockMessage myMessage2 = new BlockMessage("first", "Z");
+		System.out.println(myMessage.exclamation());
+		System.out.println(myMessage.period());
+		System.out.println(myMessage.question());
 
+		System.out.println(myMessage.a());
+		System.out.println(myMessage.b());
+		// print your letters here
+		System.out.println(myMessage.p());
+		System.out.println(myMessage.q());
+		System.out.println(myMessage.r());
 
-    System.out.println(myMessage.a());
-    System.out.println(myMessage.b());
-    //print your letters here
-<<<<<<< HEAD
-    System.out.println(myMessage.p());
-    System.out.println(myMessage.q());
-    System.out.println(myMessage.r());
+		System.out.println(myMessage.d());
+		System.out.println(myMessage.e());
+		System.out.println(myMessage.f());
+		System.out.println(myMessage.v());
+		System.out.println(myMessage.w());
+		System.out.println(myMessage.x());
 
-<<<<<<< HEAD
-=======
-	System.out.println(myMessage.d());
-	System.out.println(myMessage.e());
-	System.out.println(myMessage.f());
->>>>>>> f0cdad5503c42dbd69c72eaf86e2028edfb0c3d7
-=======
-    System.out.println(myMessage.v());
-    System.out.println(myMessage.w());
-    System.out.println(myMessage.x());
+		System.out.println(myMessage.d());
+		System.out.println(myMessage.e());
+		System.out.println(myMessage.f());
 
-	System.out.println(myMessage.d());
-	System.out.println(myMessage.e());
-	System.out.println(myMessage.f());
-
-<<<<<<< HEAD
->>>>>>> 25e8b179d447748cc9e52362cdfa785cce60f99f
-=======
-    System.out.println(myMessage.y());
-    System.out.println(myMessage2.z());
-}
-
->>>>>>> 90ffc35fb4e1aabf7934910b81230322d186b636
-    System.out.println(myMessage.vendeta());
-	System.out.println(myMessage.whyMe());
-	System.out.println(myMessage.xylophone());
-  }
+		System.out.println(myMessage.y());
+		System.out.println(myMessage2.z());
+	}
 }
